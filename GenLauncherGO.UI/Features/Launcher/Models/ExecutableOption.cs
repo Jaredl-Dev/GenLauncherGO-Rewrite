Jaredl-Dev/GenLauncherGO

feat(ui): add launcher main window and view model

GenLauncherGO.UI/Features/Launcher/Models/ExecutableOption.cs
@@ -1,0 +1,37 @@
+namespace GenLauncherGO.UI.Features.Launcher.Models;
+
+internal sealed class ExecutableOption(
+    string displayName,
+    string executableName,
+    bool isAvailable,
+    bool isBuiltIn,
+    bool isGeneralsOnline = false,
+    bool isRetail = false,
+    string groupDisplayName = "",
+    bool showGroupHeader = false)
+{
+    public string DisplayName { get; } = displayName;
+
+    public string ExecutableName { get; } = executableName;
+
+    public bool IsAvailable { get; } = isAvailable;
+
+    public bool IsUnavailable => !IsAvailable;
+
+    public bool IsBuiltIn { get; } = isBuiltIn;
+
+    public bool CanRemove => !IsBuiltIn;
+
+    public bool IsGeneralsOnline { get; } = isGeneralsOnline;
+
+    public bool IsRetail { get; } = isRetail;
+
+    public string GroupDisplayName { get; } = groupDisplayName;
+
+    public bool ShowGroupHeader { get; } = showGroupHeader;
+
+    public override string ToString()
+    {
+        return DisplayName;
+    }
+}

GenLauncherGO.UI/Features/Launcher/Models/LauncherLaunchRequest.cs
@@ -1,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using GenLauncherGO.Core.Launching.Models;
+using GenLauncherGO.Core.Mods.Models;
+
+namespace GenLauncherGO.UI.Features.Launcher.Models;
+
+internal sealed class LauncherLaunchRequest(
+    GameLaunchTargetKind targetKind,
+    string executableName,
+    bool useGeneralsOnline,
+    IReadOnlyList<LauncherContentVersion> activeVersions)
+{
+    public IReadOnlyList<LauncherContentVersion> ActiveVersions { get; } =
+        activeVersions ?? throw new ArgumentNullException(nameof(activeVersions));
+
+    public string ExecutableName { get; } = executableName ?? string.Empty;
+
+    public GameLaunchTargetKind TargetKind { get; } = targetKind;
+
+    public bool UseGeneralsOnline { get; } = useGeneralsOnline;
+}

Jaredl-Dev/GenLauncherGO-Rewrite

GenLauncherGO.UI/Features/Launcher/Models/ExecutableOption.cs
@@ -1,0 +1,37 @@
+namespace GenLauncherGO.UI.Features.Launcher.Models;
+
+internal sealed class ExecutableOption(
+    string displayName,
+    string executablePath,
+    bool isAvailable,
+    bool isBuiltIn,
+    bool isGeneralsOnline = false,
+    bool isRetail = false,
+    string groupDisplayName = "",
+    bool showGroupHeader = false)
+{
+    public string DisplayName { get; } = displayName;
+
+    public string ExecutablePath { get; } = executablePath;
+
+    public bool IsAvailable { get; } = isAvailable;
+
+    public bool IsUnavailable => !IsAvailable;
+
+    public bool IsBuiltIn { get; } = isBuiltIn;
+
+    public bool CanRemove => !IsBuiltIn;
+
+    public bool IsGeneralsOnline { get; } = isGeneralsOnline;
+
+    public bool IsRetail { get; } = isRetail;
+
+    public string GroupDisplayName { get; } = groupDisplayName;
+
+    public bool ShowGroupHeader { get; } = showGroupHeader;
+
+    public override string ToString()
+    {
+        return DisplayName;
+    }
+}

GenLauncherGO.UI/Features/Launcher/Models/LauncherLaunchRequest.cs
@@ -1,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using GenLauncherGO.Core.Launching.Models;
+using GenLauncherGO.Core.Mods.Models;
+
+namespace GenLauncherGO.UI.Features.Launcher.Models;
+
+internal sealed class LauncherLaunchRequest(
+    GameLaunchTargetKind targetKind,
+    string executablePath,
+    bool useGeneralsOnline,
+    IReadOnlyList<LauncherContentVersion> activeVersions)
+{
+    public IReadOnlyList<LauncherContentVersion> ActiveVersions { get; } =
+        activeVersions ?? throw new ArgumentNullException(nameof(activeVersions));
+
+    public string ExecutablePath { get; } = executablePath ?? string.Empty;
+
+    public GameLaunchTargetKind TargetKind { get; } = targetKind;
+
+    public bool UseGeneralsOnline { get; } = useGeneralsOnline;
+}